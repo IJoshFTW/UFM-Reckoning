ca678182f63f9622a40a62b9a908373c638fb5ca
Naomi de Ridder <naomi-ah@hotmail.com>
2015-01-06 17:06:04 +0100

    added teams to the table in OtherTeams

--- src/main/java/nl/joshuaslik/UFMReckoning/gui/game/OtherTeams.java
+++ src/main/java/nl/joshuaslik/UFMReckoning/gui/game/OtherTeams.java
@@ -2,32 +2,62 @@ package nl.joshuaslik.UFMReckoning.gui.game;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
+import javafx.util.Callback;
+import nl.joshuaslik.UFMReckoning.backend.Save;
+import nl.joshuaslik.UFMReckoning.backend.Team;
 import nl.joshuaslik.UFMReckoning.gui.Main;
 
-/**5
+/**
+ * 5
+ * 
  * @author Bryan van Wijk
  *
  */
 public class OtherTeams {
 
+	@FXML
+	private TableView<Team> otherteamstable;
+	@FXML
+	private TableColumn<Team, String> otherteam;
 
-	public static void start() throws IOException {	
-		AnchorPane scene = (AnchorPane) FXMLLoader.load(Class.class.getResource("/data/gui/pages-game/OtherTeams.fxml"));
-		AnchorPane topmenu = (AnchorPane) FXMLLoader.load(Class.class.getResource("/data/gui/pages-menu/TopMenu.fxml"));
+	@FXML
+	private void initialize() {
+
+		ObservableList<Team> teams = FXCollections.observableArrayList(Save
+				.loadTeams());
+		otherteamstable.setItems(teams);
+
+		otherteam.setCellValueFactory(new PropertyValueFactory<Team, String>(
+				"teamName"));
+
+	}
+
+	public static void start() throws IOException {
+		AnchorPane scene = (AnchorPane) FXMLLoader.load(Class.class
+				.getResource("/data/gui/pages-game/OtherTeams.fxml"));
+		AnchorPane topmenu = (AnchorPane) FXMLLoader.load(Class.class
+				.getResource("/data/gui/pages-menu/TopMenu.fxml"));
 		Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
 		topmenu.setPrefWidth(visualBounds.getWidth());
 		Label label = (Label) topmenu.lookup("#title");
 		label.setText("Other Teams");
 		Main.setCenter(scene);
 		Main.setTop(topmenu);
-		AnchorPane bottom = (AnchorPane) FXMLLoader.load(Class.class.getResource("/data/gui/pages-game/GameBottomMenuBar.fxml"));
+		AnchorPane bottom = (AnchorPane) FXMLLoader.load(Class.class
+				.getResource("/data/gui/pages-game/GameBottomMenuBar.fxml"));
 		Main.setBottom(bottom);
 	}
-	
-	
+
 }
