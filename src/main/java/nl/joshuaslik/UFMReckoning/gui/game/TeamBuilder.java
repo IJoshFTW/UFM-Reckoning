863bf39336bda1e1715bccc09cf8dcf270621c54
Sander Arjan Benoist <S.A.Benoist@student.tudelft.nl>
2015-01-06 14:38:59 +0100

    Linked the Player Team info to the TeamBuilder

--- src/main/java/nl/joshuaslik/UFMReckoning/gui/game/TeamBuilder.java
+++ src/main/java/nl/joshuaslik/UFMReckoning/gui/game/TeamBuilder.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import nl.joshuaslik.UFMReckoning.backend.Team;
 import nl.joshuaslik.UFMReckoning.gui.Main;
 
 /**
@@ -12,10 +16,23 @@ import nl.joshuaslik.UFMReckoning.gui.Main;
  *
  */
 public class TeamBuilder {
+	private static Team team;
 
 	public static void start() throws IOException {
-		Parent root = FXMLLoader.load(Class.class
+		team = MainGame.game.getUser().getTeam();
+		
+		Pane root = FXMLLoader.load(Class.class
 				.getResource("/data/gui/pages-game/TeamBuilder.fxml"));
+		
+		Image teamLogoImg = new Image("/data/base/teams/pictures/ajax.png");
+		ImageView teamLogoImgView = new ImageView();
+		teamLogoImgView.prefHeight(300);
+		teamLogoImgView.prefWidth(300);
+		teamLogoImgView.setLayoutX(10);
+		teamLogoImgView.setLayoutY(10);
+		
+		root.getChildren().addAll(teamLogoImgView);
+		
 		Scene scene = new Scene(root, 1080, 1920);
 		Main.setScene(scene);
 		Main.stage.setFullScreen(true);
